d38caccf5d59551edeed695f625bc91af59690f9
kashishpanda <71868681+kashishpanda@users.noreply.github.com>
2021-10-29 23:00:53 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import web3Modal from "web3modal"
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Home</h1>
+      <h1>hii</h1>
+    </div>
+  )
+}
---
+++ test/sample-test.js
@@ -0,0 +1,37 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("NFTMarket", function () {
+  it("Should create and execute market sales", async function () {
+    const Market = await ethers.getContractFactory("NFTMarket")
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketAdress = market.address
+
+    const NFT = await ethers.getContractFactory("NFT")
+    const nft = await NFT.deploy(marketAdress)
+    await nft.deployed()
+    const nftContractAddress = nft.address
+
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString()
+
+    const auctionPrice = ethers.utils.parseUnits('100', 'ether')
+     
+
+    await nft.createToken("https://www.mytokenlocation.com")
+    await nft.createToken("https://www.myTokenlocation2.com")
+
+    await market.createMarketItem(nftContractAddress, 1, auctionPrice, {value: listingPrice})
+    await market.createMarketItem(nftContractAddress, 2, auctionPrice, {value: listingPrice})
+
+    const [_, buyerAdress] = await ethers.getSigners()
+
+    await market.connect(buyerAdress).createMarketSale(nftContractAddress, 1, { value: auctionPrice})
+
+    const items = await market.fetchMarketItems()
+
+    console.log('items: ', items)
+
+  });
+});
